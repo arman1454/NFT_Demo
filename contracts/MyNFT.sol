c4d252af9a34ef3b863249e7c157d70c9e810f89
arman <fakidarman@gmail.com>
2024-05-04 06:47:43 -0400

    set api key url and private_key

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -16,6 +16,15 @@ contract MyNFT is ERC721URIStorage, Ownable {
     function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns(uint256){
         //here have to increment that Id _tokenIds.increment();
     
-        uint256 newItemId = _tokenIds.current()
+        uint256 newItemId = _tokenIds.current();
+
+        _mint( recipient, newItemId);
+
+        _setTokenURI(newItemId,tokenURI) //registering my itemid through the function in storage.sol
+
+        return newItemId; //finally returning the NFT id
     }
 }
+
+
+// we have to connect alchemy with our blockchain with metamask so that when we deploy our contract the fee and transaction occur in our metamask wallet only
