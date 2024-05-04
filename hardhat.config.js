a0d5cb17456f812f5c6a172836c28936821209c8
arman <fakidarman@gmail.com>
2024-05-04 10:06:44 -0400

    hardhat configured

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+require("@nomicfoundation/hardhat-ethers");
+
+const {API_URL,PRIVATE_KEY} = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.7",
+  defaultNetwork: "sepolia",
+  networks:{
+    hardhat:{},
+    sepolia:{
+      url:API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
 };
